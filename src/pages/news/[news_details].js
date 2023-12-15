2bc23e03e95ceb230c4e6d8baa8a8a932d82c8f2
naffiziqbal <f.nafiziqbal@gmail.com>
2023-12-15 22:09:35 +0600

    Get Static Paths

--- src/pages/news/[news_details].js
+++ src/pages/news/[news_details].js
@@ -1,7 +1,8 @@
 import RootLayout from "@/components/Layouts/RootLayout";
 import React from "react";
 
-const News_Details = () => {
+const News_Details = ({ news }) => {
+  console.log(news);
   return <div>This Is News Details Page</div>;
 };
 
@@ -9,4 +10,31 @@ export default News_Details;
 
 News_Details.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch("http://localhost:5000/news");
+  const data = await res.json();
+  console.log(data, "Statics Paths");
+
+  const paths = data?.map((data) => ({
+    params: { news_details: data?.id },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const { params } = context;
+  const res = await fetch(`http://localhost:5000/news/${params?.news_details}`);
+  const data = await res.json();
+  console.log(data, "StaticsProps");
+
+  return {
+    props: {
+      news: data,
+    },
+  };
 };
